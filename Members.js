74091abca949d559beff47d4f84cf0ca64c98ffb
Efraim Gabuat <ipinggxlirangg@gmail.com>
2020-09-13 13:31:13 +0800

    First Commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id:1,
+        name: 'Ep Doe',
+        email: 'epep@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name: 'Hak Doe',
+        email: 'hakdo@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:3,
+        name: 'Bak Doe',
+        email: 'bak@gmail.com',
+        status: 'inactive'
+    },
+];
+
+module.exports = members;
